Automattic/wp-cldr

commit b3584e87a114f79176b570d8b7bcc1d82990dd77

some maintenance refactoring including: - cleaning up unit tests so it doesn’t use array dereferencing which was only added in PHP 5.4 - cleaning up some inconsistency in how `pt-br` is handled - simplify how file structures are referenced so default is supplemental data items which we’ll have more of - move some file specific logic around `currencies` out of `initialize_locale_bucket` see #62. i prefer to have metric-specific logic in one place rather than scattered around class.

class.wp-cldr.php
@@ -125,7 +125,7 @@ class WP_CLDR {
 			'zh' => 'zh-Hans',
 			'als' => 'gsw',
 			'pt'	=> 'pt-PT',
-			'pt-br'	=> 'pt-BR',
+			'pt-br'	=> 'pt', // In CLDR, the root `pt` locale is Brazilian Portuguese.
 			'el-po'	=> 'el',
 			'me' => 'sr-Latn-ME',
 			'tl' => 'fil',
@@ -188,9 +188,9 @@ class WP_CLDR {
 		$base_path = __DIR__ . '/json/v' . WP_CLDR::CLDR_VERSION;
 
 		switch ( $bucket ) {
-			case 'weekData':
-			case 'telephoneCodeData':
-				$relative_path = 'cldr-core/supplemental';
+			case 'territories':
+			case 'languages':
+				$relative_path = "cldr-localenames-modern/main/$cldr_locale";
 				break;
 
 			case 'currencies':
@@ -198,7 +198,7 @@ class WP_CLDR {
 				break;
 
 			default:
-				$relative_path = "cldr-localenames-modern/main/$cldr_locale";
+				$relative_path = 'cldr-core/supplemental';
 				break;
 		}
 
@@ -233,47 +233,34 @@ class WP_CLDR {
 
 		$cldr_locale = $this->get_cldr_locale( $locale );
 
-		// Workaround for CLDR quirk that Brazilian Portuguese has the locale code "pt-BR"
-		// but JSON files and trees use "pt".
-		if ( 'pt-BR' === $cldr_locale ) {
-			$cldr_locale = 'pt';
-		}
-
-		$cldr_locale_file = self::get_cldr_json_file( $cldr_locale, $bucket );
+		$json_file = self::get_cldr_json_file( $cldr_locale, $bucket );
 
 		// If no language-country locale CLDR file, fall back to a language-only CLDR file.
-		if ( empty( $cldr_locale_file ) ) {
+		if ( empty( $json_file ) && 'supplemental' !== $bucket ) {
 			$cldr_locale = strtok( $cldr_locale, '-_' );
-			$cldr_locale_file = self::get_cldr_json_file( $cldr_locale, $bucket );
+			$json_file = self::get_cldr_json_file( $cldr_locale, $bucket );
 		}
 
 		// If no language CLDR file, fall back to English CLDR file.
-		if ( empty( $cldr_locale_file ) ) {
+		if ( empty( $json_file ) && 'supplemental' !== $bucket ) {
 			$cldr_locale = 'en';
-			$cldr_locale_file = self::get_cldr_json_file( $cldr_locale, $bucket );
+			$json_file = self::get_cldr_json_file( $cldr_locale, $bucket );
 		}
 
-		// For performance, pre-process a few items before putting into the cache.
-		switch ( $bucket ) {
-			case 'territories':
-			case 'languages':
-				$bucket_array = $cldr_locale_file['main'][ $cldr_locale ]['localeDisplayNames'][ $bucket ];
-				if ( function_exists( 'collator_create' ) ) {
-					// Sort data according to locale collation rules.
-					$coll = collator_create( $cldr_locale );
-					collator_asort( $coll, $bucket_array, Collator::SORT_STRING );
-				} else {
-					asort( $bucket_array );
-				}
-				break;
-			case 'currencies':
-				$bucket_array = $cldr_locale_file['main'][ $cldr_locale ]['numbers'][ $bucket ];
-				break;
-			default: // Covers supplemental files.
-				$bucket_array = $cldr_locale_file;
-		}
+		$this->localized[ $locale ][ $bucket ] = $json_file;
 
-		$this->localized[ $locale ][ $bucket ] = $bucket_array;
+		// For performance, sort a few items before putting into the cache.
+		if ( 'territories' === $bucket || 'languages' === $bucket ) {
+			$sorted_array = $json_file['main'][ $cldr_locale ]['localeDisplayNames'][ $bucket ];
+			if ( function_exists( 'collator_create' ) ) {
+				// Sort data according to locale collation rules.
+				$coll = collator_create( $cldr_locale );
+				collator_asort( $coll, $sorted_array, Collator::SORT_STRING );
+			} else {
+				asort( $sorted_array );
+			}
+			$this->localized[ $locale ][ $bucket ] = $sorted_array;
+		}
 
 		if ( $this->use_cache ) {
 			wp_cache_set( $cache_key, $this->localized[ $locale ][ $bucket ], WP_CLDR::CACHE_GROUP );
@@ -379,9 +366,10 @@ class WP_CLDR {
 	 * @return string The symbol for the currency in the provided locale.
 	 */
 	public function currency_symbol( $currency_code, $locale = '' ) {
-		$currencies_array = $this->get_locale_bucket( $locale, 'currencies' );
-		if ( isset( $currencies_array[ $currency_code ]['symbol'] ) ) {
-			return $currencies_array[ $currency_code ]['symbol'];
+		$json_file = $this->get_locale_bucket( $locale, 'currencies' );
+		$cldr_locale = $this->get_cldr_locale( $locale );
+		if ( isset( $json_file['main'][ $cldr_locale ]['numbers']['currencies'][ $currency_code ]['symbol'] ) ) {
+			return $json_file['main'][ $cldr_locale ]['numbers']['currencies'][ $currency_code ]['symbol'];
 		}
 		return '';
 	}
@@ -396,9 +384,10 @@ class WP_CLDR {
 	 * @return string The name of the currency in the provided locale.
 	 */
 	public function currency_name( $currency_code, $locale = '' ) {
-		$currencies_array = $this->get_locale_bucket( $locale, 'currencies' );
-		if ( isset( $currencies_array[ $currency_code ]['displayName'] ) ) {
-			return $currencies_array[ $currency_code ]['displayName'];
+		$json_file = $this->get_locale_bucket( $locale, 'currencies' );
+		$cldr_locale = $this->get_cldr_locale( $locale );
+		if ( isset( $json_file['main'][ $cldr_locale ]['numbers']['currencies'][ $currency_code ]['displayName'] ) ) {
+			return $json_file['main'][ $cldr_locale ]['numbers']['currencies'][ $currency_code ]['displayName'];
 		}
 		return '';
 	}

tests/wp-cldr-tests.php
@@ -76,7 +76,8 @@ class WP_CLDR_Tests extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( 'United States', $territories_in_english['US'] );
 
 		// Test some bad slugs.
-		$this->assertEquals( 'United States', $this->cldr->territories_by_locale( 'bad-slug' )['US'] );
+		$all_territories = $this->cldr->territories_by_locale( 'bad-slug' );
+		$this->assertEquals( 'United States', $all_territories['US'] );
 	}
 
 	public function test_languages_by_locale() {
@@ -86,7 +87,8 @@ class WP_CLDR_Tests extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( 'German', $languages_in_english['de'] );
 
 		// Test some bad slugs.
-		$this->assertEquals( 'English', $this->cldr->languages_by_locale( 'bad-slug' )['en'] );
+		$all_languages = $this->cldr->languages_by_locale( 'bad-slug' );
+		$this->assertEquals( 'English', $all_languages['en'] );
 	}
 
 	public function test_set_locale() {
@@ -147,10 +149,10 @@ class WP_CLDR_Tests extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( 'pt-PT', $this->cldr->get_cldr_locale( 'pt' ) );
 
 		// Brazilian Portuguese.
-		$this->assertEquals( 'pt-BR', $this->cldr->get_cldr_locale( 'pt-br' ) );
-		$this->assertEquals( 'pt-BR', $this->cldr->get_cldr_locale( 'pt-BR' ) );
-		$this->assertEquals( 'pt-BR', $this->cldr->get_cldr_locale( 'pt_br' ) );
-		$this->assertEquals( 'pt-BR', $this->cldr->get_cldr_locale( 'pt_BR' ) );
+		$this->assertEquals( 'pt', $this->cldr->get_cldr_locale( 'pt-br' ) );
+		$this->assertEquals( 'pt', $this->cldr->get_cldr_locale( 'pt-BR' ) );
+		$this->assertEquals( 'pt', $this->cldr->get_cldr_locale( 'pt_br' ) );
+		$this->assertEquals( 'pt', $this->cldr->get_cldr_locale( 'pt_BR' ) );
 
 		// Chinese variants.
 		$this->assertEquals( 'zh-Hans', $this->cldr->get_cldr_locale( 'zh-cn' ) );

commit e146f8dd0696d2fd9cdada98929efdca6b5e3117

fixed a regression introduced in https://github.com/Automattic/wp-cldr/commit/b3584e87a114f79176b570d8b7bcc1d82990dd77 where I tried to refactor out the case statement in `initialize_locale_bucket()` to simplify code. But after that change, data items were put into the cache based on $cldr_locale but methods like `currency_name()` tried to retrieve them based on WordPress locale. This caused us to return an empty string whenever CLDR locales differ from WordPress ones.
Bad news is that we'll need to update code in two places whenever a new data item that is locale-based is added. Good news is that caching is better since items are pre-processed before putting into cache.  Addresses #67 and partially addresses #62.

cc @jblz. Since this was breaking, and complex to debug, I went ahead and committed to master without review.

class.wp-cldr.php
@@ -247,19 +247,29 @@ class WP_CLDR {
 			$json_file = self::get_cldr_json_file( $cldr_locale, $bucket );
 		}
 
-		$this->localized[ $locale ][ $bucket ] = $json_file;
+		// Do some performance-enhancing pre-processing of data items, then put into cache
+		// organized by WordPress locale.
+		switch ( $bucket ) {
+			case 'territories':
+			case 'languages':
+				$sorted_array = $json_file['main'][ $cldr_locale ]['localeDisplayNames'][ $bucket ];
+				if ( function_exists( 'collator_create' ) ) {
+					// Sort data according to locale collation rules.
+					$coll = collator_create( $cldr_locale );
+					collator_asort( $coll, $sorted_array, Collator::SORT_STRING );
+				} else {
+					asort( $sorted_array );
+				}
+				$this->localized[ $locale ][ $bucket ] = $sorted_array;
+				break;
 
-		// For performance, sort a few items before putting into the cache.
-		if ( 'territories' === $bucket || 'languages' === $bucket ) {
-			$sorted_array = $json_file['main'][ $cldr_locale ]['localeDisplayNames'][ $bucket ];
-			if ( function_exists( 'collator_create' ) ) {
-				// Sort data according to locale collation rules.
-				$coll = collator_create( $cldr_locale );
-				collator_asort( $coll, $sorted_array, Collator::SORT_STRING );
-			} else {
-				asort( $sorted_array );
-			}
-			$this->localized[ $locale ][ $bucket ] = $sorted_array;
+			case 'currencies':
+			 	$this->localized[ $locale ][ $bucket ] = $json_file['main'][ $cldr_locale ]['numbers'][ $bucket ];
+			 	break;
+
+			default:
+				$this->localized[ $locale ][ $bucket ] = $json_file;
+				break;
 		}
 
 		if ( $this->use_cache ) {
@@ -366,10 +376,9 @@ class WP_CLDR {
 	 * @return string The symbol for the currency in the provided locale.
 	 */
 	public function currency_symbol( $currency_code, $locale = '' ) {
-		$json_file = $this->get_locale_bucket( $locale, 'currencies' );
-		$cldr_locale = $this->get_cldr_locale( $locale );
-		if ( isset( $json_file['main'][ $cldr_locale ]['numbers']['currencies'][ $currency_code ]['symbol'] ) ) {
-			return $json_file['main'][ $cldr_locale ]['numbers']['currencies'][ $currency_code ]['symbol'];
+		$currencies_array = $this->get_locale_bucket( $locale, 'currencies' );
+		if ( isset( $currencies_array[ $currency_code ]['symbol'] ) ) {
+			return $currencies_array[ $currency_code ]['symbol'];
 		}
 		return '';
 	}
@@ -384,10 +393,9 @@ class WP_CLDR {
 	 * @return string The name of the currency in the provided locale.
 	 */
 	public function currency_name( $currency_code, $locale = '' ) {
-		$json_file = $this->get_locale_bucket( $locale, 'currencies' );
-		$cldr_locale = $this->get_cldr_locale( $locale );
-		if ( isset( $json_file['main'][ $cldr_locale ]['numbers']['currencies'][ $currency_code ]['displayName'] ) ) {
-			return $json_file['main'][ $cldr_locale ]['numbers']['currencies'][ $currency_code ]['displayName'];
+		$currencies_array = $this->get_locale_bucket( $locale, 'currencies' );
+		if ( isset( $currencies_array[ $currency_code ]['displayName'] ) ) {
+			return $currencies_array[ $currency_code ]['displayName'];
 		}
 		return '';
 	}

tests/wp-cldr-tests.php
@@ -35,6 +35,7 @@ class WP_CLDR_Tests extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( '', $this->cldr->territory_name( 'bad-slug', 'fr_FR' ) );
 		$this->assertEquals( 'Africa', $this->cldr->territory_name( '002', 'badlocalecode' ) );
 		$this->assertEquals( 'Africa', $this->cldr->territory_name( '002', 'bad-locale-code' ) );
+		$this->assertEquals( 'Africa', $this->cldr->territory_name( '002', '' ) );
 	}
 
 	public function test_currency_name() {
@@ -45,6 +46,7 @@ class WP_CLDR_Tests extends PHPUnit_Framework_TestCase {
 		// Test some bad slugs.
 		$this->assertEquals( '', $this->cldr->currency_name( 'bad-slug', 'en' ) );
 		$this->assertEquals( '', $this->cldr->currency_name( '' ) );
+		$this->assertEquals( 'US Dollar', $this->cldr->currency_name( 'USD', '' ) );
 	}
 
 	public function test_currency_symbol() {
@@ -55,6 +57,7 @@ class WP_CLDR_Tests extends PHPUnit_Framework_TestCase {
 		// Test some bad slugs.
 		$this->assertEquals( '', $this->cldr->currency_symbol( 'bad-slug' ) );
 		$this->assertEquals( '', $this->cldr->currency_symbol( '' ) );
+		$this->assertEquals( '$', $this->cldr->currency_symbol( 'USD', '' ) );
 	}
 
 	public function test_language_name() {
@@ -67,6 +70,7 @@ class WP_CLDR_Tests extends PHPUnit_Framework_TestCase {
 		// Test some bad slugs.
 		$this->assertEquals( '', $this->cldr->language_name( 'bad-slug' ) );
 		$this->assertEquals( '', $this->cldr->language_name( '' ) );
+		$this->assertEquals( 'Canadian French', $this->cldr->language_name( 'fr-ca' , '' ) );
 	}
 
 	public function test_territories_by_locale() {