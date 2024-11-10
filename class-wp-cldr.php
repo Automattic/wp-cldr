fef25db59c205697f38fcd49a8c4fc36e389963a
sdnunca <sorin@php.net>
2024-11-11 00:45:43 +0200

    Update wp-cldr to version 1.3

--- class-wp-cldr.php
+++ class-wp-cldr.php
@@ -89,7 +89,7 @@ class WP_CLDR {
 	/**
 	 * The CLDR version, which the class uses to determine path to JSON files.
 	 */
-	const CLDR_VERSION = '45.0.0';
+	const CLDR_VERSION = '46.0.0';
 
 	/**
 	 * Constructs a new instance of the class, including setting defaults for locale and caching.
--- get-cldr-files.sh
+++ get-cldr-files.sh
@@ -7,7 +7,7 @@ if [ "wp-cldr" != "${PWD##*/}" ]; then
 fi
 
 # set the CLDR version
-CLDRVERSION="45.0.0"
+CLDRVERSION="46.0.0"
 
 DATA_DIR="$PWD"/data/$CLDRVERSION
 
